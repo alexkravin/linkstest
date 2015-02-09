4fc456dbaf16ade46ceaf77834a780da3fa9c5d5
alexkravin <nesterenko-aleksey@list.ru>
2015-02-09 16:41:33 +0400

    Linkcheck test project

---
+++ src/main/java/com/MyClass.java
@@ -0,0 +1,19 @@
+package com;
+
+public class MyClass
+{
+    public void foo(String str, int value, double d)
+    {
+        
+    }
+    
+    public MyClass(String some)
+    {
+        
+    }
+    
+    public MyClass(String some, int value)
+    {
+        
+    }
+}
